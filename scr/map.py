c040dee0c4771ce0657f43aa7632844d285e6819
Luis Quiroz <luisqpra@gmail.com>
2023-01-31 21:38:49 -0500

    map

--- scr/map.py
+++ scr/map.py
@@ -1,5 +1,11 @@
 from random import randint
 num_rand = [randint(0, 100) for _ in range(10)]
+num_seq = [num for num in range(10)]
+num_sqrt = list(map(lambda i: i**0.5, num_rand))
+num_sum = list(map(lambda x, y: round(x+y), num_rand, num_seq))
+# map applies a function (lambda) to each element of the list (num_rand)
 
 if __name__ == "__main__":
-    print(num_rand)
+    print("Random", num_rand)
+    print("Square root", num_sqrt)
+    print("Sum", num_sum)
