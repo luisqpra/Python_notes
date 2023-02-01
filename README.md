af2c530bddeb31e44698ee7990362e2874c5df14
Luis Quiroz <luisqpra@gmail.com>
2023-01-31 19:25:25 -0500

    explain tuples,list, and set

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Python_notes
 Notes own about python
+
+## Differences between Lists, Tuples, and Sets
+
+Data type | Mutable | Ordered | Indexing | Duplicate elements
+----------|---------|---------|--------|-------------------
+List | ✓ | ✓ | ✓ | ✓
+Tuple | x | ✓ | ✓ | ✓
+Set |✓ | x | x | x
--- scr/dictionary_comprehension.py
+++ scr/dictionary_comprehension.py
@@ -1,9 +1,13 @@
+from random import randint
 # Generate a dictionary in a single line of code
 ani = ['conejo', 'pez', 'sapo', 'perro', 'gato', 'tigre']
+countries = ['col', 'arg', 'mex', 'ven', 'bra']
 nums = [nums+1 for nums in range(len(ani))]
 nw_dic = {nums[i]: ani[i] for i in range(len(ani))}
 nwdic_O = {nums[i]: ani[i] for i in range(len(ani)) if 'o' in ani[i]}
+population = {countries[i]: randint(1, 100) for i in range(len(countries))}
 
 if __name__ == "__main__":
     print("Dictionary\n\t", nw_dic)
     print("Animals with 'o'\n\t", nwdic_O)
+    print("Population\n\t", population)
