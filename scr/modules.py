8c64d9029973e0278fbe01f89cddb5ebb6331766
Luis Quiroz <luisqpra@gmail.com>
2023-02-01 19:46:57 -0500

    package and module

---
+++ scr/modules.py
@@ -0,0 +1,11 @@
+from utils.utils import sub, add, div, mul
+
+x = 1
+y = 3
+
+if __name__ == "__main__":
+    print(f"x = {x}, y = {y}")
+    print("Subtraction", sub(x, y))
+    print("Addition", add(x, y))
+    print("Division", div(x, y))
+    print("Multiplication", mul(x, y))
---
+++ scr/utils/utils.py
@@ -0,0 +1,46 @@
+def add(x: int, y: int) -> int:
+    """
+    `add` takes two integers, `x` and `y`, and returns their sum
+    - Args:
+      x (int): int
+      y (int): int
+    - Returns:
+      The sum of x and y
+    """
+    return x+y
+
+
+def sub(x: int, y: int) -> int:
+    """
+    `sub` takes two integers, `x` and `y`, and returns their difference
+    - Args:
+      x (int): int
+      y (int): int
+    - Returns:
+      The difference between x and y
+    """
+    return x-y
+
+
+def mul(x: int, y: int) -> int:
+    """
+    `mul` takes two integers, `x` and `y`, and returns their product
+    - Args:
+      x (int): int
+      y (int): int
+    - Returns:
+      the product of x and y.
+    """
+    return x*y
+
+
+def div(x: int, y: int) -> float:
+    """
+     `div` takes two integers as arguments and returns a float.
+    - Args:
+      x (int): int
+      y (int): int
+    - Returns:
+      the value of x/y.
+    """
+    return x/y
